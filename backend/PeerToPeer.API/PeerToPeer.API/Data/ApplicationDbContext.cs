0fcdc0b1b50e4400a9d4bf75988dcca27c9536e4
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2024-09-23 12:48:45 -0300

    Created ApplicationDbContext, migration script and created the service with connection string dependecy injection

---
+++ backend/PeerToPeer.API/PeerToPeer.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeerToPeer.API.Models.Domain;
+
+namespace PeerToPeer.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
