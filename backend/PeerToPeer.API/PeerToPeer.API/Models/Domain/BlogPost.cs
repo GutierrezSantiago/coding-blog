1811eb371d4b8a7004f38ceced7ea53ddf681581
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2024-09-23 11:42:10 -0300

    Modified id property in BlogPost

--- backend/PeerToPeer.API/PeerToPeer.API/Models/Domain/BlogPost.cs
+++ backend/PeerToPeer.API/PeerToPeer.API/Models/Domain/BlogPost.cs
@@ -4,7 +4,7 @@ namespace PeerToPeer.API.Models.Domain
 {
     public class BlogPost
     {
-        public Guid id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public  string ShortDescription { get; set; }
     
